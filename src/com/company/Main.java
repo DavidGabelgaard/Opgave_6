87c5b6edb4bea9ed65f4998fa605b94c42c7bdd2
DavidGabelgaard <davi493c@edu.easj.dk>
2021-09-16 12:28:23 +0200

    after code reweiv

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,53 +2,52 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
         squareRoot(10);
 
+        System.out.println(isAbecedarian("acdf"));
 
-        System.out.println(isDoubloon("acdf"));
+        System.out.println(canSpell("qeykou" , "sadwj"));
 
-        System.out.println(canSpell("jdsaw" , "sadwj"));
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Tjek om ordet er doubloon");
+
+        String s = input.next();
+
+        if (isDoubloon(s)) {
+            System.out.println("Ordet " + s + " er doubloon");
+        } else {
+            System.out.println("Ordet " + s + " er ikke doubloon");
+        }
 
     }
 
 
     public static void squareRoot(double x) {
 
-        double Guess = x / 2.0;
+        double guess = x / 2.0;
         double difference = 1;
 
 
         while (difference > 0.0001) {
-            double OldGuess = Guess;
-            Guess = (Guess + x / Guess) / 2.0;
-            difference =  Math.abs (Math.abs(OldGuess) -Math.abs(Guess));
-            System.out.println(Guess);
+            double oldGuess = guess;
+            guess = (guess + x / guess) / 2.0;
+            difference =  Math.abs (Math.abs(oldGuess) -Math.abs(guess));
+            System.out.println(guess);
         }
     }
 
+    public static boolean isAbecedarian(String s) {
 
-    public static void gauss(double x , int n) {
+        s = s.toLowerCase();
 
-        // n can't be one
-
-        for (int i = 0; i < n - 1 ; i++) {
-        }
-
-
-
-    }
-
-
-    public static boolean isDoubloon(String s) {
-
-        for (int i = 0; i < s.length() - 1 ; i++) {
+        for (int i = 0; i < s.length()  ; i++) {
             int lastChar;
             int currChar = s.indexOf(i);
 
@@ -64,31 +63,55 @@ public class Main {
             return true;
     }
 
+    public static boolean isDoubloon(String s) {
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+            if (count != 2) {
+                return false;
+            }
+        }
+    return true;
+    }
+
+
 
     public static boolean canSpell(String tiles, String word) {
 
-        boolean TileMissing = false;
+        tiles = tiles.toLowerCase();
 
-        List<Character> CTiles = new ArrayList<>();
+        boolean tileMissing = false;
+
+        List<Character> cTiles = new ArrayList<>();
             // adds all the tiles to a list
         for (int i = 0; i < tiles.length() ; i++) {
-            CTiles.add(tiles.charAt(i));
+            cTiles.add(tiles.charAt(i));
         }
         // Run a for loop for the word we are trying to spell
         for (int i = 0; i < word.length() ; i++) {
             // Hold the letter in the word we are trying to spell up against the tiles we have
-            for (int j = 0; j < CTiles.size(); j++) {
-                if (word.charAt(i) == CTiles.get(j)) {
-                    CTiles.remove(j);
-                    TileMissing = false;
+            for (int j = 0; j < cTiles.size(); j++) {
+                if (word.charAt(i) == cTiles.get(j)) {
+                    cTiles.remove(j);
+                    tileMissing = false;
                     break;
                 } else {
-                    TileMissing = true;
+                    tileMissing = true;
                 }
             }
             // inside the loop for the word
             // Check if the current letter was found and if not stop and return false
-            if (TileMissing) {
+            if (tileMissing) {
                 return false;
             }
         }
